3fbba7c45cafa80f4b3a29937894f0645b68ac95
Nicola <nicola.sebastianelli@studio.unibo.it>
2017-09-06 13:05:43 +0200

    bocciate di fillo

--- Room8/View/PartiForm.Designer.cs
+++ Room8/View/PartiForm.Designer.cs
@@ -35,10 +35,6 @@
             this._annullaButton = new System.Windows.Forms.Button();
             this._formTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this._scrollPanel = new System.Windows.Forms.Panel();
-            this._partiTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this._splitContainer = new System.Windows.Forms.SplitContainer();
-            this._utenteLabel = new System.Windows.Forms.Label();
-            this._numericUpDown = new System.Windows.Forms.NumericUpDown();
             this._vScrollBar = new System.Windows.Forms.VScrollBar();
             this._partiLabel = new System.Windows.Forms.Label();
             this._errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
@@ -46,12 +42,6 @@
             this._confirmPanel.SuspendLayout();
             this._formTableLayoutPanel.SuspendLayout();
             this._scrollPanel.SuspendLayout();
-            this._partiTableLayoutPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this._splitContainer)).BeginInit();
-            this._splitContainer.Panel1.SuspendLayout();
-            this._splitContainer.Panel2.SuspendLayout();
-            this._splitContainer.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this._numericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._errorProvider)).BeginInit();
             this.SuspendLayout();
             // 
@@ -120,7 +110,6 @@
             // 
             // _scrollPanel
             // 
-            this._scrollPanel.Controls.Add(this._partiTableLayoutPanel);
             this._scrollPanel.Controls.Add(this._vScrollBar);
             this._scrollPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this._scrollPanel.Location = new System.Drawing.Point(0, 0);
@@ -128,57 +117,6 @@
             this._scrollPanel.Name = "_scrollPanel";
             this._scrollPanel.Size = new System.Drawing.Size(327, 132);
             this._scrollPanel.TabIndex = 0;
-            // 
-            // _partiTableLayoutPanel
-            // 
-            this._partiTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._partiTableLayoutPanel.ColumnCount = 1;
-            this._partiTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this._partiTableLayoutPanel.Controls.Add(this._splitContainer, 0, 0);
-            this._partiTableLayoutPanel.Location = new System.Drawing.Point(3, 0);
-            this._partiTableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
-            this._partiTableLayoutPanel.Name = "_partiTableLayoutPanel";
-            this._partiTableLayoutPanel.RowCount = 1;
-            this._partiTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this._partiTableLayoutPanel.Size = new System.Drawing.Size(307, 42);
-            this._partiTableLayoutPanel.TabIndex = 2;
-            // 
-            // _splitContainer
-            // 
-            this._splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._splitContainer.Location = new System.Drawing.Point(3, 3);
-            this._splitContainer.Name = "_splitContainer";
-            // 
-            // _splitContainer.Panel1
-            // 
-            this._splitContainer.Panel1.Controls.Add(this._utenteLabel);
-            // 
-            // _splitContainer.Panel2
-            // 
-            this._splitContainer.Panel2.Controls.Add(this._numericUpDown);
-            this._splitContainer.Size = new System.Drawing.Size(301, 36);
-            this._splitContainer.SplitterDistance = 205;
-            this._splitContainer.TabIndex = 0;
-            // 
-            // _utenteLabel
-            // 
-            this._utenteLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._utenteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._utenteLabel.Location = new System.Drawing.Point(0, 0);
-            this._utenteLabel.Name = "_utenteLabel";
-            this._utenteLabel.Size = new System.Drawing.Size(205, 36);
-            this._utenteLabel.TabIndex = 0;
-            this._utenteLabel.Text = "Nome utente";
-            this._utenteLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // _numericUpDown
-            // 
-            this._numericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this._numericUpDown.Location = new System.Drawing.Point(13, 10);
-            this._numericUpDown.Name = "_numericUpDown";
-            this._numericUpDown.Size = new System.Drawing.Size(66, 20);
-            this._numericUpDown.TabIndex = 1;
             // 
             // _vScrollBar
             // 
@@ -219,12 +157,6 @@
             this._confirmPanel.ResumeLayout(false);
             this._formTableLayoutPanel.ResumeLayout(false);
             this._scrollPanel.ResumeLayout(false);
-            this._partiTableLayoutPanel.ResumeLayout(false);
-            this._splitContainer.Panel1.ResumeLayout(false);
-            this._splitContainer.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this._splitContainer)).EndInit();
-            this._splitContainer.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this._numericUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._errorProvider)).EndInit();
             this.ResumeLayout(false);
 
@@ -241,9 +173,5 @@
         private System.Windows.Forms.Panel _scrollPanel;
         private System.Windows.Forms.VScrollBar _vScrollBar;
         private System.Windows.Forms.ErrorProvider _errorProvider;
-        private System.Windows.Forms.TableLayoutPanel _partiTableLayoutPanel;
-        private System.Windows.Forms.SplitContainer _splitContainer;
-        private System.Windows.Forms.Label _utenteLabel;
-        private System.Windows.Forms.NumericUpDown _numericUpDown;
     }
 }
